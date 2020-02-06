ade85c4a5f1a0c67b81861d6490725db9d324e91
gbgkogkas <gbgkogkas@gmail.com>
2020-02-06 23:50:42 +0200

    Clear Session after download

--- download.php
+++ download.php
@@ -9,5 +9,7 @@
     $filePointer = fopen($_SESSION['next-download']['location'],"rb");
     fpassthru($filePointer);
 
+    unset($_SESSION['next-download']);
+
     echo '<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>';
     echo '<script>$(document).ready(function() {setTimeout(function() {parent.closeFrame(' . $_SESSION['next-download']['name'] . ');},120000)})</script>';
